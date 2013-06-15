4bddebce8b8670ccf3d83eb58f610e3713333feb
Kyle Spraggs <theman@spiffyjr.me>
2013-06-14 19:51:11 -0500

    Updates.

--- config/module.config.php
+++ config/module.config.php
@@ -1,14 +1,6 @@
 <?php
 
 return array(
-    'doctrine' => array(
-        'orm_default' => array(
-            'drivers' => array(
-                'ZfcUserRemember\Entity' => 'zfc_user_doctrine_orm',
-            )
-        )
-    ),
-
     'service_manager' => include 'service.config.php',
 
     'zfc_user' => array(
